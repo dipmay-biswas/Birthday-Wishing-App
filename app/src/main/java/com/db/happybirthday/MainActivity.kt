041313756fad2d081892f066a83de5fd64bf387a
Dipmay Biswas <117085298+DipmayBiswas@users.noreply.github.com>
2023-10-25 21:39:36 +0530

    Birthday Greeting Cards generating App

---
+++ app/src/main/java/com/db/happybirthday/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.db.happybirthday
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun CreateBirthdayCard(view: View) {
+        val name = nameinput.editableText.toString()
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name)
+        startActivity(intent)
+    }
+}
